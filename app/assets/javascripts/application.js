51e74a0609ec79039bd8af490d75739a366ae0b3
vivek <vivek@vivek-HP-ProBook-4540s.(none)>
2014-08-22 00:18:59 +0530

    add template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require main
 //= require turbolinks
-//= require twitter/bootstrap
 //= require_tree .
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+	$('#login_modal').click(function(){$('#signinModal').modal('show');})
+	$('#sign_up_modal').click(function(){$('#signupModal').modal('show');})
+
+
+});
